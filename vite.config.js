79234d68207d0ad58b67aa0a1decb3f41e2388c1
Kebede <johnzeeze@yahoo.com>
2025-11-18 08:23:40 -0800

    Fix vite.config.js file

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "/final-project-news-explorer/",
+  base: "/",
   plugins: [react()],
     server: {
       port: 4000,
